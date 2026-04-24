b9d307dca409b8b267df3d1bf5bc7389110bd14d
Weiming Zhou <weimingchess108@gmail.com>
2026-04-24 23:31:00 +0000

    Add lane detector

---
+++ setup.py
@@ -0,0 +1,29 @@
+import glob
+from setuptools import setup
+
+package_name = 'final_challenge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/final_challenge/launch', glob.glob('launch/*.xml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rss2026-7',
+    maintainer_email='rss2026-7@mit.edu',
+    description='RSS 2026 Final Challenge — lane following and boating school',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lane_detector = final_challenge.lane_detector:main',
+            'boundary_pure_pursuit = final_challenge.lane_follower:main',
+            'test_lane_publisher = final_challenge.test_lane_publisher:main',
+        ],
+    },
+)
