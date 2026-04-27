abb1e1e7e4b1e1f40ef5e4acfd98e8064706c1a8
Weiming Zhou <weimingchess108@gmail.com>
2026-04-27 19:22:49 +0000

    Update setup.py to include computer vision as well

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'final_challenge'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, package_name + '.computer_vision'],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
